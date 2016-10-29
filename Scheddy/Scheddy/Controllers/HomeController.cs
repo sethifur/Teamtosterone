cff329a570248dd08c66fe480bfc46961ec3c6f0
RXtr <RobbieXtreme@gmail.com>
2016-10-29 10:17:57 -0600

    Initial commit. default project.

---
+++ Scheddy/Scheddy/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scheddy.Controllers
+{
+    public class HomeController : Controller
+    {
+        //default controller. this is for the home page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Scheddy/Scheddy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scheddy.Startup))]
+namespace Scheddy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
