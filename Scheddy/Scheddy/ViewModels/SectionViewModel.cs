b7275771990796273f06b73d24e085ee8585bf4e
RXtr <RobbieXtreme@gmail.com>
2016-11-03 21:06:47 -0600

    Added base for Section View Model. Will flesh it out later. Also removed test code from Home Index. Also started work on the SectionController to AddSection. Update will be very similar

---
+++ Scheddy/Scheddy/ViewModels/SectionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Scheddy.Models;
+
+namespace Scheddy.ViewModels
+{
+    public class SectionViewModel
+    {
+        public int? CourseId { get; set; }
+        public int? ClassroomId { get; set; }
+        public int? InstructorId { get; set; }
+        public int? ScheduleId { get; set; }
+        public int? CRN { get; set; }
+        public DateTime StarTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string DaysTaught { get; set; }
+        public string numSeats { get; set; }
+    }
+}
